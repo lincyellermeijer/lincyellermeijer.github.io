fe61d46b229d30102c85845b589550ddef3b4140
Lincy Ellermeijer <lincy_95@hotmail.com>
2016-03-29 22:32:50 +0200

    Code formatting beautified

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,56 +1,56 @@
-function loadPage(){
-	// Fetch the existing objects
-	objects = getObjects();
+function loadPage() {
+    // Fetch the existing objects
+    objects = getObjects();
 
-	// Clear the list
-	$('#items').find('li').remove();
+    // Clear the list
+    $('#items').find('li').remove();
 
-	// Add every object to the objects list
-	$.each(objects, function(index, item){
-		element = '<li>'+item.title+'</li>';
-		
-		$('#items').append(element);
-	});
-  
-   $('#items').listview();
-   $('#items').listview("refresh");
+    // Add every object to the objects list
+    $.each(objects, function (index, item) {
+        element = '<li>' + item.title + '</li>';
+
+        $('#items').append(element);
+    });
+
+    $('#items').listview();
+    $('#items').listview("refresh");
 }
 
 // CREATE
 
-function add(){
-	// Retrieve the entered form data
-	var title = $('[name="item"]').val();
-	// Fetch the existing objects
-	var objects = getObjects();
-	// Push the new item into the existing list
-	objects.push({
-		title: title
-	});
-	// Store the new list
-	saveObjects(objects);
-	// Reload the page to show the new objects
-	window.location.reload();
+function add() {
+    // Retrieve the entered form data
+    var title = $('[name="item"]').val();
+    // Fetch the existing objects
+    var objects = getObjects();
+    // Push the new item into the existing list
+    objects.push({
+        title: title
+    });
+    // Store the new list
+    saveObjects(objects);
+    // Reload the page to show the new objects
+    window.location.reload();
 }
 
 // READ
 
-function getObjects(){
-	// See if objects is inside localStorage
-	if (localStorage.getItem("objects")){
-		// If yes, then load the objects
-		objects = JSON.parse(localStorage.getItem("objects"));
-	}else{
-		// Make a new array of objects
-		objects = new Array();
+function getObjects() {
+    // See if objects is inside localStorage
+    if (localStorage.getItem("objects")) {
+        // If yes, then load the objects
+        objects = JSON.parse(localStorage.getItem("objects"));
+    } else {
+        // Make a new array of objects
+        objects = new Array();
 
-	}
-	return objects;
+    }
+    return objects;
 }
 
-function saveObjects(objects){
-	// Save the list into localStorage
-	localStorage.setItem("objects", JSON.stringify(objects));
+function saveObjects(objects) {
+    // Save the list into localStorage
+    localStorage.setItem("objects", JSON.stringify(objects));
 }
 
 // UPDATE
@@ -59,14 +59,13 @@ function saveObjects(objects){
 
 // DELETE
 
-function deleteAll(){
+function deleteAll() {
     // Fetch the existing objects
-	objects = getObjects();
+    objects = getObjects();
 
-	// Clear the list
-	$('#items').find('li').remove();
+    // Clear the list
+    $('#items').find('li').remove();
 
     $('#items').listview();
     $('#items').listview("refresh");
 }
-
